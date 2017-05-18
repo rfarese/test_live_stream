01409bb921d305590efb8a3343fa112d3776a046
Robert Farese <robertfarese@yahoo.com>
2017-05-18 19:03:08 -0400

    removed rest-client gem

--- Gemfile
+++ Gemfile
@@ -7,7 +7,6 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'foundation-rails'
-gem 'rest-client'
 
 group :development do
   gem 'listen', '~> 3.0.5'
--- app/models/video_stream.rb
+++ app/models/video_stream.rb
@@ -9,11 +9,12 @@ class VideoStream
     @response = self.build
   end
 
-  def build 
+  def build
     uri = URI.parse("https://api.bcovlive.io/v1/jobs")
     request = Net::HTTP::Post.new(uri)
+    binding.pry
     request.content_type = "application/json"
-    request["X-Api-Key"] = "YOUR_API_KEY_HERE"
+    request["X-Api-Key"] = ENV["CLIENT_SECRET"]
     request.body = JSON.dump({
       "live_sliding_window_duration" => 57600,
       "outputs" => [
@@ -65,51 +66,3 @@ class VideoStream
     end
   end
 end
-
-
-# the request we want
-
-# curl -X "POST" "https://api.bcovlive.io/v1/jobs" \
-#      -H "X-API-KEY: YOUR_API_KEY_HERE" \
-#      -H "Content-Type: application/json" \
-#      -d $'{
-#   "live_sliding_window_duration": 57600,
-#   "outputs": [
-#     {
-#       "height": 720,
-#       "video_bitrate": 2100,
-#       "segment_seconds": 4,
-#       "keyframe_interval": 60,
-#       "width": 1280,
-#       "video_codec": "h264",
-#       "label": "hls720p",
-#       "h264_profile": "high",
-#       "live_stream": true
-#     },
-#     {
-#       "height": 540,
-#       "video_bitrate": 1500,
-#       "segment_seconds": 4,
-#       "keyframe_interval": 60,
-#       "width": 960,
-#       "video_codec": "h264",
-#       "label": "hls540p",
-#       "h264_profile": "main",
-#       "live_stream": true
-#     },
-#     {
-#       "height": 360,
-#       "video_bitrate": 800,
-#       "segment_seconds": 4,
-#       "keyframe_interval": 60,
-#       "width": 640,
-#       "video_codec": "h264",
-#       "label": "hls360p",
-#       "h264_profile": "main",
-#       "live_stream": true
-#     }
-#   ],
-#   "region": "ap-southeast-2",
-#   "reconnect_time": 1800,
-#   "live_stream": true
-# }'
